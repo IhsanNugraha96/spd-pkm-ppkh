322d3546aa57a2b112592121d464dbd17ffdd35d
Ihsan Nugraha <ihsannugraha96@gmail.com>
2023-07-10 21:53:38 +0700

    create page login & dashboard

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Routing register
+Route::get('/register', 'App\Http\Controllers\RegisterController@index')->middleware('guest');
+Route::post('/register', 'App\Http\Controllers\RegisterController@store')->name('register')->middleware('guest');
+
+//Routing login
+Route::get('/', 'App\Http\Controllers\AuthController@index')->middleware('guest');
+Route::get('/login', 'App\Http\Controllers\AuthController@index')->name('auth.login');
+Route::post('/authenticate', 'App\Http\Controllers\AuthController@authenticate')->name('authenticate')->middleware('guest');
+Route::get('/logout', 'App\Http\Controllers\AuthController@logout')->name('auth.logout')->middleware('auth');
+
+//Routing dashboard
+Route::get('/dashboard', 'App\Http\Controllers\DashboardController@index')->name('dashboard')->middleware('auth');
